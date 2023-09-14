0b7b483b0704d9411e6bd0793b38b55962ecf5a5
pluure <p.luure2@gmail.com>
2023-09-13 17:42:12 -0700

    simple shell

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio>
+#include <stdlib>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
+
+#define MAX_INPUT_SIZE 1024
+
+void shell_pmt(char **av, char **env);
+
+#endif

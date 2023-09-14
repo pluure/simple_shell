339e440a8db21b042fa306bf8172f65c73d2692c
pluure <p.luure2@gmail.com>
2023-09-13 18:15:18 -0700

    simple shell

---
+++ mainshell.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+/**
+ * main - main shell entry
+ * Return: nothing
+ */
+int main(void)
+{
+	char input[MAX_INPUT_SIZE];
+	FILE *cmd_output;
+	char cmd_output_line[MAX_INPUT_SIZE];
+
+	while (1)
+	{
+		printf("Myshell> ");
+		if (fgets(input, sizeof(input), stdin) == NULL)
+		{
+			printf("Exiting\n");
+			break;
+		}
+		input[strlen(input) - 1] = '\0';
+
+		if (strlen(input) == 0)
+		{
+			continue;
+		}
+		cmd_output = popen(input, "r");
+		if (cmd_output == NULL)
+		{
+			perror(" ");
+			continue;
+		}
+		while (fgets(cmd_output_line, sizeof(cmd_output_line), cmd_output) != NULL)
+		{
+			printf("%s", cmd_output_line);
+		}
+		pclose(cmd_output);
+	}
+
+	return (0);
+}
--- shell.h
+++ shell.h
@@ -1,8 +1,8 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio>
-#include <stdlib>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
